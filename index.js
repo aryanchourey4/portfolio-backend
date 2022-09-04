4167b0469fb7fd88853ce4f3337e5f86f0026d25
Aryan Chourey <aryanchourey4@gmail.com>
2022-09-05 01:11:06 +0530

    bakc

--- index.js
+++ index.js
@@ -23,7 +23,11 @@ const corsOptions = {
 }
 dotenv.config();
 
-app.use(cors(corsOptions))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
