bbb97bdf4c029f42bb5916afe75969056c0e295d
Aryan Chourey <aryanchourey4@gmail.com>
2022-09-05 01:08:33 +0530

    back

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ const whitelist = process.env.frontendURL || "http://localhost:3000"
 console.log(whitelist)
 const corsOptions = {
   origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
+    // if (!origin || whitelist.indexOf(origin) !== -1) {
+    //   callback(null, true)
+    // } else {
+    //   callback(new Error("Not allowed by CORS"))
+    // }
+    callback(null, true)
   },
   credentials: true,
 }
